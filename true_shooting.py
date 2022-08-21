544a0da87c6fef716752adf74844e87c394e53bf
gabriel1200 <grg2117@columbia.edu>
2022-08-20 22:15:26 -0500

    changed port

--- true_shooting.py
+++ true_shooting.py
@@ -380,7 +380,7 @@ def update_output(value):
 
 # Run app and display result inline in the notebook
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(port=8050)
 
 
 # In[ ]:
